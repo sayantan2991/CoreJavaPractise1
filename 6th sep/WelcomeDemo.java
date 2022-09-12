5817eb5c8839cb03be80d5cfa4f468c26195d725
Sayantan Naskar <113057264+sayantan2991@users.noreply.github.com>
2022-09-12 07:46:15 +0530

    Add files via upload

---
+++ 6th sep/WelcomeDemo.java
@@ -0,0 +1,22 @@
+/*
+Program: print a argument "Hello + name"
+@author: sayantan naskar
+@date: 6th sep2022
+*/
+
+//decalraing a class
+class  WelcomeDemo
+{
+//caling main
+
+public static void main(String[] args)
+{
+System.out.println("Welcome"+ args[0]);
+System.out.println("Welcome"+ args[1]);
+	
+}
+//end main
+
+}
+//end of class
+
