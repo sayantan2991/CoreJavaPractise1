4924912bf77bc051b93217c3765445baeb28903e
Sayantan Naskar <113057264+sayantan2991@users.noreply.github.com>
2022-09-12 06:57:50 +0530

    Add files via upload

---
+++ 9th sep/SwitchDemo2.java
@@ -0,0 +1,35 @@
+/*
+@class name: Printing the day on the basis of value 
+@author: sayantan naskar
+@date: 9th sep 2022
+*/
+//declaring class 
+
+class SwitchDemo2
+{
+public static void main(String [] args)
+{
+	char grade='B';
+	switch(grade)
+	{
+		case 'A': System.out.println("Pass: Excellen");
+			break;
+		case 'B': System.out.println("Pass: very good");
+			break;
+		case 'C': System.out.println("Pass good");
+			break;
+		case 'D': System.out.println("Pass: can do better");
+			break;
+		case 'F': System.out.println("Fail");
+			break;
+		default: System.out.println("wrong input");
+		
+	}
+	//end of switch
+
+	
+}
+//end of main 
+}	
+	//end of class 
+	
